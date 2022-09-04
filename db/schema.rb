7e6427a7df66b727715583f1a7f3f9af771d6323
rubens_cordeiro <rubensccordeiro@outlook.com>
2022-09-04 20:35:32 -0300

    Recreated env

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_07_07_170951) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,4 +36,5 @@ ActiveRecord::Schema.define(version: 2022_07_07_170951) do
     t.string "email"
     t.boolean "mailable", default: false, null: false
   end
+
 end
